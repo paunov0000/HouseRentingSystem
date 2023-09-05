35efe5e5ff9e5cff5e787f6ce2430a513aafc771
paunov0000 <legendarygriefer96@gmail.com>
2023-09-05 19:18:00 +0300

    added category entity and did some QOL things

---
+++ HouseRentingSystem.Infrastructure/Data/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Infrastructure.Data.Entities
+{
+    public class Category
+    {
+        public Category()
+        {
+            Houses = new List<House>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+
+        public IEnumerable<House> Houses { get; set; }
+    }
+}
