3a1d5ee294c122c09cb7070ba46b41bcb8e6bb3c
paunov0000 <legendarygriefer96@gmail.com>
2024-03-25 22:00:15 +0200

    removed default template

--- houserentingsystem.client/src/App.jsx
+++ houserentingsystem.client/src/App.jsx
@@ -1,33 +1,12 @@
 import './App.css';
 
-import DisplayForecast from './components/DisplaytForecast';
-
 function App() {
-
-    //const contents = forecasts === undefined
-    //    ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-    //    : 
 
     return (
         <div>
             <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            <table className="table table-striped" aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <DisplayForecast />
-                </tbody>
-            </table>
         </div>
     );
-
 
 }
 
